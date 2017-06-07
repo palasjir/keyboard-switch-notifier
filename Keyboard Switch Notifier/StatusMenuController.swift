27f15da6a55cc7fa9c8c7e7cc0b197b020cdd40c
Jiří Palas <palas.jiri@gmail.com>
2017-06-07 22:32:54 +0200

    proper size of status menu icon and clean assets

--- Keyboard Switch Notifier/StatusMenuController.swift
+++ Keyboard Switch Notifier/StatusMenuController.swift
@@ -14,7 +14,7 @@ class StatusMenuController: NSObject {
     let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
     
     override func awakeFromNib() {
-        let icon = NSImage(named: "StatusBarButtonImage")
+        let icon = NSImage(named: "statusMenu")
         icon?.isTemplate = true // best for dark mode
         statusItem.image = icon
         statusItem.menu = statusMenu
